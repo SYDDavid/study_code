23057e799fdce740cad3cd269dca060cd50a2f59
TreeHole <2522337205@gmail.com>
2024-01-12 17:39:05 +0800

    提交 学习成果

--- 2_python进阶/10.可执行文件.py
+++ 2_python进阶/10.可执行文件.py
@@ -3,3 +3,10 @@
 # 步骤三：pyinstaller byhy.py --workpath d:\pybuild  --distpath d:\pybuild\dist
 
 ## 结果文件存放在：d:\pybuild\dist
+
+'''
+    --workpath      配置工作目录
+    --distpath      配置结果目录
+    --noconsole     不显示黑色dos界面
+    --onefile       结果仅为一个可执行文件
+'''
